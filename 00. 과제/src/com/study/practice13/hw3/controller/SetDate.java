d5facf43aa55a8584e418ca11f780d04cd6e3361
chkm306 <chkm306@gmail.com>
2022-02-11 10:03:38 +0900

    220211

---
+++ 00. 과제/src/com/study/practice13/hw3/controller/SetDate.java
@@ -0,0 +1,19 @@
+package com.study.practice13.hw3.controller;
+
+public class SetDate {
+	
+	public SetDate() {
+	}
+	
+	public String todayPrint() {
+		return null;
+		
+	}
+	
+	public String setDay() {
+		return null;
+		
+	}
+	
+
+}
